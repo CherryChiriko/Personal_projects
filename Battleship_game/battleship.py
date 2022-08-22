58ff158167c995116da2854c8a86322672f9b558
Chiriko <sofia.digennarox@gmail.com>
2022-08-22 23:31:49 +0200

    Rewriting done, first ship can be placed

--- Battleship_game/battleship.py
+++ Battleship_game/battleship.py
@@ -3,9 +3,8 @@ import string
 import random
 
 class Board:
-    def __init__(self, dim):
+    def __init__(self, dim):  # building the basic board
         self.dim = dim
-        # self.board = np.zeros((dim+1,dim+1), dtype=str)
         self.board = np.full((dim+1,dim+1), " ")
 
         for i, letter in enumerate(string.ascii_uppercase):
@@ -15,71 +14,46 @@ class Board:
             self.board[i][0] = str(i)
     
     def place_ship(self):
-        ship = Ship(2, self)
-        ship.get_ship_elements(self)
+        ship = Ship(4)
+        elements = ship.set_ship_elements(self)
         x_points = [(1,0), (-1,0), (1,1),(0,1),(0,-1),(-1,-1),(-1,1),(1,-1)]
-        for el in ship.ship_elements:
+        for el in elements:
             el_x, el_y = el
             self.board[el_x][el_y] = "o"
 
             for dx,dy in x_points:
                 x = el_x + dx; y = el_y + dy
-                print(x,y)
                 if x> self.dim or y>self.dim : continue
                 if self.board[x][y] != "o" and x>0 and y>0 : self.board[x][y] = "x"
     
     
         
 class Ship:
-    def __init__(self, dim, board):
+    def __init__(self, dim):
         self.dim = dim
-        self.position_i = (random.randint(1, board.dim),random.randint(1,board.dim))
-        self.ship_elements = []
 
-    def direction(self, n):
-        direction_switch = {1: (-1,0), 2: (1,0), 3: (0,-1), 4:(0,1)}  #1: left, 2: right, 3: down, 4: up
-        self.direc = direction_switch.get(n)
+    def set_ship_elements(self, board):
+        ship_elements = []
 
-    def set_position_f(self):
-        n = random.randint(1,4)
-        self.direction(n)
+        direction_switch = {0: (0, 1), 1: (1,0)}  #0: right, 1: down
+        n = random.randint(0,1)
+        dx, dy = direction_switch.get(n)
+        r1, r2 = random.randint(1, board.dim),random.randint(1,board.dim-self.dim+1)
+        x_i = r1 if not n else r2                    #if down x in [1,10], y in [1,11-dim]; if right x in [1,11-dim], y in [1,10]
+        y_i = r2 if not n else r1
+        x_f = x_i + dx * (self.dim-1)
+        y_f = y_i + dy * (self.dim-1)
+        position_i = (x_i, y_i) 
+        print("(",x_i, ",",y_i,")","(",x_f, ",",y_f,")")
 
-        x_i, y_i = self.position_i
-        x,y = self.direc
-        x *= (self.dim-1); y*= (self.dim-1)
-        x_f = x + x_i; y_f = y + y_i;
-        if x_f > 0 and y_f >0 : self.position_f = (x_f, y_f)
-        else: self.position_f = (x_i - x, y_i - y)   #temporary solution, it is not completely random if we do it in this way
-
-        # self.position_f = tuple(map(lambda t : t[0]*self.dim + t[1], zip(self.direction(n), self.position_i)))
-        #self.position_f = tuple(map(lambda a,b : a*self.dim + b, self.direc, self.position_i))
-    
-    def get_ship_elements(self, board):
-        self.ship_elements.append(self.position_i)
-        if self.dim == 1: return self.ship_elements
-        self.set_position_f()
+        ship_elements.append(position_i)
+        if self.dim == 1: return ship_elements
+        
         for i in range(1, self.dim):
-            dx, dy = self.direc
-            x_i, y_i = self.position_i
             x = x_i + i*dx; y = y_i + i*dy
-            self.ship_elements.append((x,y))
-            print(self.ship_elements)
-        self.move_elements(board)
-
-    def move_elements(self, board): 
-        for i, el in enumerate(self.ship_elements):
-            x, y = el
-            x_i, y_i = self.position_i
-            if x> board.dim or y> board.dim or x<0 or y<0 or board.board[x][y] in ["x","o"]: 
-                x1, y1 = self.ship_elements[-2]
-                x2, y2 = self.ship_elements[-1]
-                dx, dy = (x2-x1, y2-y1)
-                if (x2 + dx)> board.dim or (y2 + dy)> board.dim or (x2 + dx)<0 or (y2 + dy)<0 or board.board[(x2 + dx)][(y2 + dy)] in ["x","o"]:
-                    self.ship_elements[i] = (x_i - dx, y_i -dy)
-                else:
-                    self.ship_elements[i] = (x2 + dx, y2 +dy)
-                print(self.ship_elements[i])
-                print("I'm moving ", el, "to ", self.ship_elements[i])
+            ship_elements.append((x,y))
+            print(ship_elements)
+        return ship_elements
         
 
 
